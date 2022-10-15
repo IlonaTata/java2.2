74711da93afcf0162bdfdd96d45342bc4443a324
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-10-15 19:58:09 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int check = 200;
-        int amount = 1100;
-        int bonus = amount/100;
-
-
+        int amount = 1000;
+        int bonus = amount / 100;
         int balance;
-        if (amount >=- 1000) {
-             balance = check + amount + bonus;
+        if (amount > 1000) {
+            balance = check + amount + bonus;
+        } else {
+            balance = check + amount;
         }
-        else {
-             balance = check + amount;
-    }
         System.out.println(balance);
     }
 }
